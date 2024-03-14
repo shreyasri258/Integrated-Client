355e7681e3546ffe5198b1b05aac12e2fb68033f
shreyasri258 <kertishreya2508@gmail.com>
2024-03-14 15:22:59 +0530

    Added ExamReport Page with report cards

---
+++ src/Admin/utils/ExamReportCard.jsx
@@ -0,0 +1,25 @@
+// Card component for displaying submission information
+function ExamReportCard({ name, score, malpractices }) {
+    let nameColor="bg-yellow-400";
+    let cardColor = "bg-yellow-200";
+    if (malpractices >= 7) {
+      cardColor = "bg-red-200";
+      nameColor="bg-red-400";
+    } else if (malpractices === 0) {
+      cardColor = "bg-green-200";
+      nameColor="bg-green-400";
+    }
+  
+    return (
+      <div className={`shadow-md rounded p-4 w-72 h-48 border ${cardColor}`}>
+        <h2 className={`font-bold ${nameColor} text-center rounded text-lg`}>{name}</h2>
+        
+        <div className="mt-5">
+          <p>Score: {score}</p>
+          <p className="mt-2">Malpractices: {malpractices}</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default ExamReportCard;
