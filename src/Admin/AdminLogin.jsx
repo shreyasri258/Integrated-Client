9f1bafaff4eb53ecd6e56aeef3d1c346ebbd19d4
shreyasri258 <kertishreya2508@gmail.com>
2024-03-20 20:36:53 +0530

    Added Password UI

--- src/Admin/AdminLogin.jsx
+++ src/Admin/AdminLogin.jsx
@@ -5,6 +5,7 @@ import { AdminContext } from "../contextCalls/adminContext/AdminContext";
 import { login } from "../contextCalls/adminContext/apiCalls";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Import eye icons
 
 
 const TeacherLogin = () => {
@@ -12,6 +13,7 @@ const TeacherLogin = () => {
   const { dispatch } = useContext(AdminContext);
   const [inputValues, setInputValues] = useState({});
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleInputChange = (fieldName, event) => {
     const value = event.target.value
@@ -21,62 +23,76 @@ const TeacherLogin = () => {
     }));
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
 
-  
-
-  // Pass navigate to the login function
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      await login(inputValues, dispatch, navigate); // Pass navigate here
+      await login(inputValues, dispatch, navigate);
       setIsLoggedIn(true);
     } catch (err) {
       console.log(err);
     }
   };
   
-   
-     const isFormValid = () => {
-    // Check if email and password fields are not empty and email is valid
+  const isFormValid = () => {
     return inputValues.email && inputValues.email.trim() !== "" &&
            inputValues.password && inputValues.password.trim() !== "" &&
            /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputValues.email);
   };
 
-
   return (
-    <div className="user-login" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+    <div className="user-login flex flex-col items-center justify-center h-screen">
       <ToastContainer />
-           <div className="logo" style={{ marginBottom: '20px' }}>
-             <img src={Icon} alt="proctorpal-logo" />
-           </div>
-           <div className="login-form" style={{ border: '1px solid #ccc', borderRadius: '14px', padding: '20px', boxShadow: '0 0 20px rgba(0, 0, 0, 0.2)' }}>
-             <h1 className="title-heading" style={{ textAlign: 'center', marginBottom: '20px' }}>Examiner Login</h1>
-             <form onSubmit={handleLogin}>
-               <div className="input-fields" style={{ display: 'flex', flexDirection: 'column', marginBottom: '20px' }}>
-                 <input
-                  type="email"
-                  placeholder="Email"
-                  value={inputValues.email || ""}
-                  onChange={(event) => handleInputChange("email", event)}
-                  style={{ marginBottom: '10px' }}
-                />
-                  {!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputValues.email) && ( <span className='text-center' style={{ color: 'red', fontSize: '12px', marginBottom: '5px' }}>Invalid email format </span> )}
-          
-                <input
-                  type="password"
-                  placeholder="Password"
-                  value={inputValues.password || ""}
-                  onChange={(event) => handleInputChange("password", event)}
-                  style={{ marginBottom: '10px' }}
-                />
-              </div>
-              <button type="submit" disabled={!isFormValid()} style={{ width: '100%' }}>Login</button>
-            </form>
+      <div className="logo mb-20">
+        <img src={Icon} alt="proctorpal-logo" />
+      </div>
+      <div className="login-form border border-gray-300 rounded-lg p-5 shadow-md">
+        <h1 className="title-heading text-center mb-5">Examiner Login</h1>
+        <form onSubmit={handleLogin}>
+          <div className="input-fields flex flex-col mb-5 relative">
+          <input
+  type="email"
+  placeholder="Email"
+  value={inputValues.email || ""}
+  onChange={(value) => handleInputChange("email", value)}
+  style={{
+    marginBottom: '10px',
+    border: !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputValues.email) ? '1px solid red' : '1px solid #ccc',
+    transition: 'border-color 0.3s',
+  }}
+/>
+{!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputValues.email) && (
+  <span className='text-center' style={{ color: 'red', fontSize: '12px', marginBottom: '5px' }}> Invalid email format </span>
+)}
+             <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                value={inputValues.password || ""}
+                onChange={(event) => handleInputChange("password", event)}
+                className="mb-5 pr-12"
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                onMouseDown={(e) => e.preventDefault()}
+                className="absolute right-2 top-2 cursor-pointer bg-transparent border-none text-black"
+            
+               >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+           
+
+            <button type="submit" disabled={!isFormValid()} className="w-full bg-blue-500 text-white py-2 rounded-md">Login</button>
           </div>
-        </div>
-      
- );
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default TeacherLogin;
--- src/User/StudentLogin.jsx
+++ src/User/StudentLogin.jsx
@@ -5,6 +5,7 @@ import { StudentContext } from "../contextCalls/studentContext/StudentContext";
 import { login } from "../contextCalls/studentContext/apiCalls";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const StudentLogin = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -12,6 +13,12 @@ const StudentLogin = () => {
   const [inputValues, setInputValues] = useState({});
   const navigate = useNavigate();
 
+  const [showPassword, setShowPassword] = useState(false);
+
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
   const handleInputChange = (fieldName, value) => {
     setInputValues(prevValues => ({
       ...prevValues,
@@ -55,21 +62,38 @@ const StudentLogin = () => {
         <h1 className="title-heading" style={{ textAlign: 'center', marginBottom: '20px' }}>Examinee Login</h1>
         <form onSubmit={handleLogin}>
           <div className="input-fields" style={{ display: 'flex', flexDirection: 'column', marginBottom: '20px' }}>
-            <input
-              type="email"
-              placeholder="Email"
-              value={inputValues.email || ""}
-              onChange={(value) => handleInputChange("email", value)}
-              style={{ marginBottom: '10px' }}
-            />
-            {!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputValues.email) && ( <span className='text-center' style={{ color: 'red', fontSize: '12px', marginBottom: '5px' }}> Invalid email format </span> )}
-            <input
-              type="password"
-              placeholder="Password"
-              value={inputValues.password || ""}
-              onChange={(value) => handleInputChange("password", value)}
-              style={{ marginBottom: '10px' }}
-            />
+          <input
+  type="email"
+  placeholder="Email"
+  value={inputValues.email || ""}
+  onChange={(value) => handleInputChange("email", value)}
+  style={{
+    marginBottom: '10px',
+    border: !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputValues.email) ? '1px solid red' : '1px solid #ccc',
+    transition: 'border-color 0.3s',
+  }}
+/>
+{!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputValues.email) && (
+  <span className='text-center' style={{ color: 'red', fontSize: '12px', marginBottom: '5px' }}> Invalid email format </span>
+)}
+ <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                value={inputValues.password || ""}
+                onChange={(event) => handleInputChange("password", event)}
+                className="mb-5 pr-12"
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                onMouseDown={(e) => e.preventDefault()}
+                className="absolute right-2 top-2 cursor-pointer bg-transparent border-none text-black"
+            
+               >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
           </div>
           <button type="submit" disabled={!isFormValid()} style={{ width: '100%' }} onMouseEnter={handleButtonHover}>Login</button>
         </form>
