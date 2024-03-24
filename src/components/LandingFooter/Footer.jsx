538e70f8ce0b0f9e8f7dbd0215084a6501ec9ad3
Akshitha123-24 <mengansaiakshitha03@gmail.com>
2024-03-25 03:22:31 +0530

    Updated Footer

--- src/components/LandingFooter/Footer.jsx
+++ src/components/LandingFooter/Footer.jsx
@@ -1,125 +1,136 @@
-import React from 'react';
-import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import React from "react";
+import {
+    Box,
+    FooterContainer,
+    Row,
+    Column,
+    FooterLink,
+    Heading,
+} from "./FooterStyles";
+import HomeIcon from '@mui/icons-material/Home';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import GoogleIcon from '@mui/icons-material/Google';
 
-export default function Footer() {
-  return (
-    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
-      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-        <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks:</span>
-        </div>
-
-        <div>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="facebook-f" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="twitter" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="google" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="instagram" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="linkedin" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="github" />
-          </a>
-        </div>
-      </section>
-
-      <section className=''>
-        <MDBContainer className='text-center text-md-start mt-5'>
-          <MDBRow className='mt-3'>
-            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                Company name
-              </h6>
-              <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit.
-              </p>
-            </MDBCol>
-
-            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Angular
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  React
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Vue
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Laravel
-                </a>
-              </p>
-            </MDBCol>
-
-            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Help
-                </a>
-              </p>
-            </MDBCol>
-
-            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              <p>
-                <MDBIcon icon="home" className="me-2" />
-                New York, NY 10012, US
-              </p>
-              <p>
-                <MDBIcon icon="envelope" className="me-3" />
-                info@example.com
-              </p>
-              <p>
-                <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
-              </p>
-              <p>
-                <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
-              </p>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </section>
-
-      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-        © 2021 Copyright:
-        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
-        </a>
-      </div>
-    </MDBFooter>
-  );
-}
+ 
+const Footer = () => {
+    return (
+        <Box>
+            
+            <FooterContainer>
+                <Row style={{ alignItems: "center" }}>
+                    <Column>
+                        <span style={{  marginBottom:"40px" }}>Get connected with us on social networks:</span>
+                    </Column>
+                    <Row style={{ display: "flex", alignItems: "center",marginBottom:"40px" }}>
+                        <FacebookIcon style={{ marginRight: "5px" }} />
+                        <XIcon style={{ marginRight: "5px" }} />
+                        <InstagramIcon style={{ marginRight: "5px" }} />
+                        <LinkedInIcon style={{ marginRight: "5px" }} />
+                        <GitHubIcon style={{ marginRight: "5px" }} />
+                        <GoogleIcon style={{ marginRight: "5px" }} />
+                    </Row>
+        
+                </Row>
+            </FooterContainer>
+            <FooterContainer>
+                <Row>
+                    <Column>
+                        <Heading>COMPANY NAME</Heading>
+                        <p>Here you can use rows and columns to organize your footer content.Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </Column>
+                    <Column>
+                        <Heading>PRODUCTS</Heading>
+                        <FooterLink href="#">
+                            Angular
+                        </FooterLink>
+                        <FooterLink href="#">
+                            React
+                        </FooterLink>
+                        <FooterLink href="#">
+                            Vue
+                        </FooterLink>
+                        <FooterLink href="#">
+                            Laravel
+                        </FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>USEFUL LINKS</Heading>
+                        <FooterLink href="#">
+                            Pricing
+                        </FooterLink>
+                        <FooterLink href="#">
+                            Settings
+                        </FooterLink>
+                        <FooterLink href="#">
+                            Orders
+                        </FooterLink>
+                        <FooterLink href="#">
+                            Help
+                        </FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>CONTACT</Heading>
+                        
+                        <FooterLink href="#">
+                        <HomeIcon />
+                          <i className="fab fa-facebook-f">
+                                <span
+                                    style={{
+                                        marginLeft: "10px",
+                                    }}
+                                >
+                                    
+                                    New York, NY 10012, US
+                                </span>
+                            </i>
+                        </FooterLink>
+                        <FooterLink href="#">
+                            <EmailIcon/>
+                            <i className="fab fa-instagram">
+                                <span
+                                    style={{
+                                        marginLeft: "10px",
+                                    }}
+                                >
+                                    info@example.com
+                                </span>
+                            </i>
+                        </FooterLink>
+                        <FooterLink href="#">
+                            <PhoneIcon/>
+                            <i className="fab fa-twitter">
+                                <span
+                                    style={{
+                                        marginLeft: "10px",
+                                    }}
+                                >
+                                    + 01 234 567 88
+                                </span>
+                            </i>
+                        </FooterLink>
+                        <FooterLink href="#">
+                            <PhoneAndroidIcon/>
+                            <i className="fab fa-youtube">
+                                <span
+                                    style={{
+                                        marginLeft: "10px",
+                                    }}
+                                >
+                                    + 01 234 567 89
+                                </span>
+                            </i>
+                        </FooterLink>
+                    </Column>
+                </Row>
+            </FooterContainer>
+        </Box>
+    );
+};
+export default Footer;
